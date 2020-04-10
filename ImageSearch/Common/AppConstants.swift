93eab302485adada19cec7f315ad570f7f4eddc5
Denis Simon <denis.v.simon@gmail.com>
2020-04-10 04:26:07 +0300

    Update app constants

--- ImageSearch/Common/AppConstants.swift
+++ ImageSearch/Common/AppConstants.swift
@@ -19,16 +19,13 @@ struct AppConstants {
     struct FlickrAPI {
         // "8ca55bca1384f45ab957b7618afc6ecc"
         static let ApiKey = ""._8.c.a._5._5.b.c.a._1._3._8._4.f._4._5.a.b._9._5._7.b._7._6._1._8.a.f.c._6.e.c.c
-        static let PhotosPerRequest = 20 // up to 20 photos
         static let BaseURL = "https://api.flickr.com/services/"
-        static let HotListCount = 30 // up to 30 trending tags
+        static let PhotosPerRequest = 20 // up to 20 photos
     }
     
     struct ImageCollection {
         static let ItemsPerRow: Float = 2
         static let BaseImageWidth: Float = 240
-        static let ReuseHeaderIdentifier = "CollectionViewHeader"
-        static let ReuseCellIdentifier = "CollectionViewCell"
         static let VerticleSpace: Float = 35
         static let HorizontalSpace: Float = 20
     }
