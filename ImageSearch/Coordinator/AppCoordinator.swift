0c78b7ca40a63134dae8fa7ec1e8c31090678c06
Denis <denis.v.simon@gmail.com>
2024-01-15 02:31:33 +0500

    Update AppCoordinator.swift

--- ImageSearch/Coordinator/AppCoordinator.swift
+++ ImageSearch/Coordinator/AppCoordinator.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// Handles the app's flow
 class AppCoordinator: Coordinator {
     
     // MARK: - Properties
@@ -21,7 +20,6 @@ class AppCoordinator: Coordinator {
     
     // MARK: - Methods
     func start(completionHandler: CoordinatorStartCompletionHandler?) {
-        // We can use MainCoordinator as a coordinator for the entire app, as well as for a first tab (with a separate UINavigationController each) or even just for an app feature such as ImagesFeature (and in this case it's better to rename MainCoordinator to the ImagesFeatureCoordinator and to move it to the ImagesFeature folder)
         let mainCoordinator = dependencyContainer.makeMainCoordinator(navigationController: navigationController)
         mainCoordinator.start(completionHandler: nil)
     }
