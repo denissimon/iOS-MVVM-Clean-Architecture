f1ec93f373e574df7cfe05dee2d91bff841a4ffa
Denis <denis.v.simon@gmail.com>
2024-02-07 00:15:57 +0500

    Added Alertable protocol

---
+++ ImageSearch/Common/Utils/Protocols/Alertable.swift
@@ -0,0 +1,33 @@
+//
+//  Alertable.swift
+//  ImageSearch
+//
+//  Created by Denis Simon on 02/06/2024.
+//
+
+import UIKit
+
+protocol Alertable {}
+extension Alertable where Self: UIViewController {
+    
+    func showAlert(title: String, message: String, style: UIAlertController.Style = .alert, okHandler: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "Ok", style: .default) { _ in
+            if okHandler != nil { okHandler!() }
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    func makeToast(message: String, duration: TimeInterval = AppConfiguration.Other.toastDuration, position: ToastPosition = .bottom) {
+        self.view.makeToast(message, duration: duration, position: position)
+    }
+    
+    func makeToastActivity(position: ToastPosition = .center) {
+        self.view.makeToastActivity(position)
+    }
+    
+    func hideToastActivity() {
+        self.view.hideToastActivity()
+    }
+}
