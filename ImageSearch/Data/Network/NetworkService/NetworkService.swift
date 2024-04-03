14d3d615160c4aab73a0f58896ac7f97259b9105
Denis <denis.v.simon@gmail.com>
2024-04-04 04:32:21 +0500

    Added new NetworkService methods that return a status code along with the result

--- ImageSearch/Data/Network/NetworkService/NetworkService.swift
+++ ImageSearch/Data/Network/NetworkService/NetworkService.swift
@@ -15,9 +15,14 @@ struct NetworkError: Error {
 
 protocol NetworkServiceType {
     var urlSession: URLSession { get }
+    
     func request(_ endpoint: EndpointType, completion: @escaping (Result<Data?, NetworkError>) -> Void) -> NetworkCancellable?
     func request<T: Decodable>(_ endpoint: EndpointType, type: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) -> NetworkCancellable?
     func fetchFile(url: URL, completion: @escaping (Data?) -> Void) -> NetworkCancellable?
+    
+    func requestWithStatusCode(_ endpoint: EndpointType, completion: @escaping (Result<(result: Data?, statusCode: Int?), NetworkError>) -> Void) -> NetworkCancellable?
+    func requestWithStatusCode<T: Decodable>(_ endpoint: EndpointType, type: T.Type, completion: @escaping (Result<(result: T, statusCode: Int?), NetworkError>) -> Void) -> NetworkCancellable?
+    func fetchFileWithStatusCode(url: URL, completion: @escaping ((result: Data?, statusCode: Int?)) -> Void) -> NetworkCancellable?
 }
 
 class NetworkService: NetworkServiceType {
@@ -94,6 +99,85 @@ class NetworkService: NetworkServiceType {
                 return
             }
             completion(data)
+        }
+        
+        dataTask.resume()
+        
+        return dataTask
+    }
+    
+    func requestWithStatusCode(_ endpoint: EndpointType, completion: @escaping (Result<(result: Data?, statusCode: Int?), NetworkError>) -> Void) -> NetworkCancellable? {
+        
+        guard let url = URL(string: endpoint.baseURL + endpoint.path) else {
+            completion(.failure(NetworkError(error: nil, statusCode: nil, data: nil)))
+            return nil
+        }
+        
+        let request = RequestFactory.request(url: url, method: endpoint.method, params: endpoint.params)
+        log("\nNetworkService requestWithStatusCode \(endpoint.method.rawValue), url: \(url)")
+        
+        let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
+            let response = response as? HTTPURLResponse
+            let statusCode = response?.statusCode
+            
+            if error == nil {
+                completion(.success((data, statusCode)))
+                return
+            }
+            
+            completion(.failure(NetworkError(error: error, statusCode: statusCode, data: data)))
+        }
+        
+        dataTask.resume()
+        
+        return dataTask
+    }
+    
+    func requestWithStatusCode<T: Decodable>(_ endpoint: EndpointType, type: T.Type, completion: @escaping (Result<(result: T, statusCode: Int?), NetworkError>) -> Void) -> NetworkCancellable? {
+        
+        guard let url = URL(string: endpoint.baseURL + endpoint.path) else {
+            completion(.failure(NetworkError(error: nil, statusCode: nil, data: nil)))
+            return nil
+        }
+        
+        let request = RequestFactory.request(url: url, method: endpoint.method, params: endpoint.params)
+        log("\nNetworkService requestWithStatusCode<T: Decodable> \(endpoint.method.rawValue), url: \(url)")
+        
+        let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
+            let response = response as? HTTPURLResponse
+            let statusCode = response?.statusCode
+            
+            if error == nil {
+                guard let data = data, let decoded = ResponseDecodable.decode(type, data: data) else {
+                    completion(.failure(NetworkError(error: nil, statusCode: statusCode, data: data)))
+                    return
+                }
+                completion(.success((decoded, statusCode)))
+                return
+            }
+            
+            completion(.failure(NetworkError(error: error, statusCode: statusCode, data: data)))
+        }
+
+        dataTask.resume()
+        
+        return dataTask
+    }
+    
+    func fetchFileWithStatusCode(url: URL, completion: @escaping ((result: Data?, statusCode: Int?)) -> Void) -> NetworkCancellable? {
+        let request = RequestFactory.request(url: url, method: .GET, params: nil)
+        log("\nNetworkService fetchFileWithStatusCode: \(url)")
+     
+        let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
+            let response = response as? HTTPURLResponse
+            let statusCode = response?.statusCode
+            
+            guard let data = data, !data.isEmpty, error == nil else {
+                completion((nil, statusCode))
+                return
+            }
+            
+            completion((data, statusCode))
         }
         
         dataTask.resume()
