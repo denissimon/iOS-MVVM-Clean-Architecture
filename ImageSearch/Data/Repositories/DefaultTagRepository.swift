35bd1be7ea73f1e71d39c610208bc7c3bf954d04
Denis <denis.v.simon@gmail.com>
2024-05-21 20:47:03 +0300

    Update DefaultTagRepository.swift

--- ImageSearch/Data/Repositories/DefaultTagRepository.swift
+++ ImageSearch/Data/Repositories/DefaultTagRepository.swift
@@ -13,11 +13,11 @@ class DefaultTagRepository: TagRepository {
         do {
             let tags = try await apiInteractor.request(endpoint, type: Tags.self)
             if tags.stat != "ok" {
-                return .failure(NetworkError(error: nil, statusCode: nil, data: nil))
+                return .failure(NetworkError())
             }
             return .success(tags)
         } catch {
-            return .failure(NetworkError(error: error, statusCode: nil, data: nil))
+            return .failure(NetworkError(error: error))
         }
     }
 }
