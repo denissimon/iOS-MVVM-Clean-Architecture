314f4f9a670d7913c82ca139a125a260658786de
Denis <denis.v.simon@gmail.com>
2024-07-16 03:33:09 +0300

    Update ImageCachingService.swift

--- ImageSearch/Domain/Interfaces/Services/ImageCachingService.swift
+++ ImageSearch/Domain/Interfaces/Services/ImageCachingService.swift
@@ -2,7 +2,6 @@ import Foundation
 
 protocol ImageCachingService: Actor {
     var cachingTask: Task<Void, Never>? { get }
-    var searchIdsFromCache: Set<String> { get }
     var didProcess: Event<[ImageSearchResults]> { get }
     func cacheIfNecessary(_ data: [ImageSearchResults]) async
     func getCachedImages(searchId: String) async -> [Image]?
