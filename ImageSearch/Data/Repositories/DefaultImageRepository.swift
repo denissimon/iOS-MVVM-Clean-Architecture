d5b264bd34485a2618e8d4d37b3361aecfac80c3
Denis <denis.v.simon@gmail.com>
2024-08-02 01:22:57 +0300

    Update DefaultImageRepository.swift

--- ImageSearch/Data/Repositories/DefaultImageRepository.swift
+++ ImageSearch/Data/Repositories/DefaultImageRepository.swift
@@ -23,30 +23,26 @@ class DefaultImageRepository: ImageRepository {
     }
     
     func prepareImages(_ imagesData: Data?) async -> [Image]? {
-        do {
-            guard
-                let imagesData = imagesData, !imagesData.isEmpty,
-                let resultsDictionary = try JSONSerialization.jsonObject(with: imagesData) as? [String: AnyObject],
-                let stat = resultsDictionary["stat"] as? String
-                else { return nil }
+        guard
+            let imagesData = imagesData, !imagesData.isEmpty,
+            let resultsDictionary = try? JSONSerialization.jsonObject(with: imagesData) as? [String: AnyObject],
+            let stat = resultsDictionary["stat"] as? String
+            else { return nil }
 
-            if stat != "ok" { return nil }
-            
-            guard
-                let container = resultsDictionary["photos"] as? [String: AnyObject],
-                let photos = container["photo"] as? [[String: AnyObject]]
-                else { return nil }
-            
-            let imagesFound: [Image] = photos.compactMap { imageDict in
-                return Image(flickrParams: imageDict)
-            }
-            
-            guard !imagesFound.isEmpty else { return nil }
-            
-            return imagesFound
-        } catch {
-            return nil
+        if stat != "ok" { return nil }
+        
+        guard
+            let container = resultsDictionary["photos"] as? [String: AnyObject],
+            let photos = container["photo"] as? [[String: AnyObject]]
+            else { return nil }
+        
+        let imagesArr: [Image] = photos.compactMap { imageDict in
+            return Image(flickrParams: imageDict)
         }
+        
+        guard !imagesArr.isEmpty else { return nil }
+        
+        return imagesArr
     }
     
     func getImage(url: URL) async -> Data? {
