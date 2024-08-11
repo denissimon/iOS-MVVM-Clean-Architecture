1ac0ee87e1d54b754d1b435ba5e8efa11dd969bc
Denis <denis.v.simon@gmail.com>
2024-08-11 23:35:44 +0300

    Update DIContainer.swift

--- ImageSearch/Coordinator/DIContainer/DIContainer.swift
+++ ImageSearch/Coordinator/DIContainer/DIContainer.swift
@@ -20,37 +20,37 @@ class DIContainer {
     // MARK: - Repositories
     
     func makeImageRepository() -> ImageRepository {
-        return DefaultImageRepository(apiInteractor: apiInteractor, imageDBInteractor: imageDBInteractor)
+        DefaultImageRepository(apiInteractor: apiInteractor, imageDBInteractor: imageDBInteractor)
     }
     
     func makeTagRepository() -> TagRepository {
-        return DefaultTagRepository(apiInteractor: apiInteractor)
+        DefaultTagRepository(apiInteractor: apiInteractor)
     }
     
     // MARK: - Use Cases
     
-    lazy var searchImagesUseCase: SearchImagesUseCase = {
-        return DefaultSearchImagesUseCase(imageRepository: makeImageRepository())
-    }()
+    func makeSearchImagesUseCase() -> SearchImagesUseCase {
+        DefaultSearchImagesUseCase(imageRepository: makeImageRepository())
+    }
     
-    lazy var getBigImageUseCase: GetBigImageUseCase = {
-        return DefaultGetBigImageUseCase(imageRepository: makeImageRepository())
-    }()
+    func makeGetBigImageUseCase() -> GetBigImageUseCase {
+        DefaultGetBigImageUseCase(imageRepository: makeImageRepository())
+    }
     
-    lazy var getHotTagsUseCase: GetHotTagsUseCase = {
-        return DefaultGetHotTagsUseCase(tagRepository: makeTagRepository())
-    }()
+    func makeGetHotTagsUseCase() -> GetHotTagsUseCase {
+        DefaultGetHotTagsUseCase(tagRepository: makeTagRepository())
+    }
     
     // MARK: - Services
     
     lazy var imageCachingService: ImageCachingService = {
-        return DefaultImageCachingService(imageRepository: makeImageRepository())
+        DefaultImageCachingService(imageRepository: makeImageRepository())
     }()
     
     // MARK: - Flow Coordinators
     
     func makeMainCoordinator(navigationController: UINavigationController) -> MainCoordinator {
-        return MainCoordinator(navigationController: navigationController, dependencyContainer: self)
+        MainCoordinator(navigationController: navigationController, dependencyContainer: self)
     }
 }
 
@@ -60,17 +60,17 @@ extension DIContainer: MainCoordinatorDIContainer {
     // MARK: - View Controllers
     
     func makeImageSearchViewController(actions: ImageSearchCoordinatorActions) -> ImageSearchViewController {
-        let viewModel = DefaultImageSearchViewModel(searchImagesUseCase: searchImagesUseCase, imageCachingService: imageCachingService)
+        let viewModel = DefaultImageSearchViewModel(searchImagesUseCase: makeSearchImagesUseCase(), imageCachingService: imageCachingService)
         return ImageSearchViewController.instantiate(viewModel: viewModel, actions: actions)
     }
     
     func makeImageDetailsViewController(image: Image, imageQuery: ImageQuery) -> ImageDetailsViewController {
-        let viewModel = DefaultImageDetailsViewModel(getBigImageUseCase: getBigImageUseCase, image: image, imageQuery: imageQuery)
+        let viewModel = DefaultImageDetailsViewModel(getBigImageUseCase: makeGetBigImageUseCase(), image: image, imageQuery: imageQuery)
         return ImageDetailsViewController.instantiate(viewModel: viewModel)
     }
     
     func makeHotTagsViewController(actions: HotTagsCoordinatorActions, didSelect: Event<ImageQuery>) -> HotTagsViewController {
-        let viewModel = DefaultHotTagsViewModel(getHotTagsUseCase: getHotTagsUseCase, didSelect: didSelect)
+        let viewModel = DefaultHotTagsViewModel(getHotTagsUseCase: makeGetHotTagsUseCase(), didSelect: didSelect)
         return HotTagsViewController.instantiate(viewModel: viewModel, actions: actions)
     }
 }
