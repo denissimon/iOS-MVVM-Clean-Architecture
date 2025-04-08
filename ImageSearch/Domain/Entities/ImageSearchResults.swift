a57be703664073bc2bc818fd88881947d6484e3a
Denis <denis.v.simon@gmail.com>
2025-04-08 14:07:40 +0300

    Update ImageSearchResults.swift

--- ImageSearch/Domain/Entities/ImageSearchResults.swift
+++ ImageSearch/Domain/Entities/ImageSearchResults.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-typealias ImageSearchResultsListItemVM = ImageSearchResults
+protocol ImageSearchResultsListItemVM {
+    var id: String { get }
+    var searchQuery: ImageQuery { get }
+    var searchResults_: [ImageListItemVM] { get set }
+}
 
-class ImageSearchResults {
+class ImageSearchResults: ImageSearchResultsListItemVM {
     let id: String
     let searchQuery: ImageQuery
     var searchResults: [Image]
@@ -11,5 +15,12 @@ class ImageSearchResults {
         self.id = id
         self.searchQuery = searchQuery
         self.searchResults = searchResults
+    }
+}
+
+extension ImageSearchResults {
+    var searchResults_: [ImageListItemVM] {
+        get { searchResults }
+        set { searchResults = newValue as! [Image] }
     }
 }
