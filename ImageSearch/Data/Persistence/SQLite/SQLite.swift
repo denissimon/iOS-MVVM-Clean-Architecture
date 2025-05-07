e4dd41411b830fbbfdab39e6a033bd58ec0b5b7a
Denis <denis.v.simon@gmail.com>
2025-05-07 04:19:27 +0300

    Update SQLite.swift

--- ImageSearch/Data/Persistence/SQLite/SQLite.swift
+++ ImageSearch/Data/Persistence/SQLite/SQLite.swift
@@ -527,7 +527,7 @@ class SQLite: SQLiteType {
         let columnCount = sqlite3_column_count(sqlStatement)
         for index in 0..<columnCount {
             if let columnName = sqlite3_column_name(sqlStatement, index) {
-                if let validatedColumnName = String(validatingUTF8: columnName) {
+                if let validatedColumnName = String(validatingCString: columnName) {
                     columnNamesToReturn.append(validatedColumnName)
                 } else {
                     throw SQLiteError.Column(getErrorMessage(dbPointer: dbPointer))
