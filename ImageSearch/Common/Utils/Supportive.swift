d90ad6454fa4ad414afb10ad02952c5f50df0ada
Denis <denis.v.simon@gmail.com>
2025-05-07 21:14:31 +0300

    Update Supportive.swift
    Added isRunningUnitTests()

--- ImageSearch/Common/Utils/Supportive.swift
+++ ImageSearch/Common/Utils/Supportive.swift
@@ -1,21 +1,15 @@
 import UIKit
 
 class Supportive {
+    
     static func toUIImage(from data: Data) -> UIImage? {
         if let image = UIImage(data: data) {
             return image
         }
         return nil
     }
-}
-
-class DeepCopier {
-    static func copy<T: Codable>(of object: T) -> T? {
-       do {
-           let json = try JSONEncoder().encode(object)
-           return try JSONDecoder().decode(T.self, from: json)
-       } catch {
-           return nil
-       }
+    
+    static func isRunningUnitTests() -> Bool {
+        NSClassFromString("XCTest") != nil
     }
 }
