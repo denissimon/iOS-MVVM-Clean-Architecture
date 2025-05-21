0e695c3a8469ed17050c1a09ca09f736c3c1281b
Denis <denis.v.simon@gmail.com>
2025-05-21 23:41:30 +0300

    Update Extensions.swift

--- ImageSearch/Common/Utils/Extensions.swift
+++ ImageSearch/Common/Utils/Extensions.swift
@@ -4,23 +4,23 @@ import SwiftUI
 extension String {
     
     func encodeURIComponent() -> String? {
-        return self.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        self.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
     }
     
     func decodeURIComponent() -> String? {
-        return self.removingPercentEncoding
+        self.removingPercentEncoding
     }
 }
 
 extension Array where Element == ImageWrapper {
     func toUIImageArray() -> [UIImage] {
-        return self.map { $0.uiImage ?? UIImage() }
+        self.map { $0.uiImage ?? UIImage() }
     }
 }
 
 extension UIApplication {
     var keyWindow: UIWindow? {
-        return self.connectedScenes
+        self.connectedScenes
             .filter { $0.activationState == .foregroundActive }
             .first(where: { $0 is UIWindowScene })
             .flatMap({ $0 as? UIWindowScene })?.windows
@@ -30,15 +30,11 @@ extension UIApplication {
 
 extension UIWindow {
     static var isLandscape: Bool {
-        if #available(iOS 13.0, *) {
-            return UIApplication.shared
-                .keyWindow?
-                .windowScene?
-                .interfaceOrientation
-                .isLandscape ?? false
-        } else {
-            return UIApplication.shared.statusBarOrientation.isLandscape
-        }
+        UIApplication.shared
+            .keyWindow?
+            .windowScene?
+            .interfaceOrientation
+            .isLandscape ?? false
     }
 }
 
