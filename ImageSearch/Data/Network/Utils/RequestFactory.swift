b57d9448566d69bfe200a350262c5e30657e96f6
Denis <denis.v.simon@gmail.com>
2025-05-27 05:36:47 +0300

    Update RequestFactory.swift

--- ImageSearch/Data/Network/Utils/RequestFactory.swift
+++ ImageSearch/Data/Network/Utils/RequestFactory.swift
@@ -8,18 +8,7 @@ class RequestFactory {
         request.httpMethod = endpoint.method.rawValue
         
         if let params = endpoint.params {
-            if let httpBody = params.httpBody {
-                switch httpBody {
-                case is Data:
-                    request.httpBody = httpBody as? Data
-                case is Encodable:
-                    if let encodable = httpBody as? Encodable {
-                        request.httpBody = encodable.encode()
-                    }
-                default:
-                    break
-                }
-            }
+            request.httpBody = params.httpBody
             if params.cachePolicy != nil { request.cachePolicy = params.cachePolicy! }
             if params.timeoutInterval != nil { request.timeoutInterval = params.timeoutInterval! }
             if params.headerValues != nil {
