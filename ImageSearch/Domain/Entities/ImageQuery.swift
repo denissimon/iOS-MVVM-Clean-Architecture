aa021b99e7adf25edbb100e82c05bdfb62e5ef23
Denis <denis.v.simon@gmail.com>
2025-05-27 05:41:13 +0300

    Moved query validation logic to the ImageQuery

--- ImageSearch/Domain/Entities/ImageQuery.swift
+++ ImageSearch/Domain/Entities/ImageQuery.swift
@@ -2,4 +2,9 @@ import Foundation
 
 struct ImageQuery: Equatable {
     let query: String
+    
+    init?(query: String) {
+        guard !query.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return nil }
+        self.query = query
+    }
 }
